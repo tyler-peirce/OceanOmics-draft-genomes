174bbb55c2e6cad399fed152ec8056383a2ed914
LaurenHuet <88010555+LaurenHuet@users.noreply.github.com>
2024-03-04 10:53:09 +0800

    Create slurm.sh
    added slurm script

---
+++ assembly/slurm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash --login
+#SBATCH --account=
+#SBATCH --job-name=
+#SBATCH --partition=work
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=24:00:00
+#SBATCH --mem=12G
+
+#SBATCH --export=NONE
+
+#-----------------
+#Loading the required modules
+
+module load nextflow/23.04.3
+
+unset SBATCH_EXPORT
+
+nextflow run assembly.nf -c nextflow.config -profile setonix -resume -disable-jobs-cancellation -with-report [run name].html
