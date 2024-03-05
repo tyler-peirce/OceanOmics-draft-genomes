b74e56d204c6939df38a1bd4f34e599d3b342368
LaurenHuet <88010555+LaurenHuet@users.noreply.github.com>
2024-03-05 14:25:22 +0800

    Create NCBI_find_contam_farm.sh
    find contam farming

---
+++ Decontamination/NCBI_find_contam_farm.sh
@@ -0,0 +1,13 @@
+rundir=
+DATE= [this is the date of the sequencing run as appears in the assembly file]
+scripts=
+for i in $rundir/*; do
+if [ -d "$i" ]; then
+sample=$(basename "$i")
+assembly=$(basename "$i.ilmn.$DATE")
+rundir=
+fasta="$rundir/$sample/assemblies/genome/$assembly.v129mh.fasta"
+sbatch $scripts/NCBI_find_contam.sh "$sample" "$rundir" "$assembly" "$fasta"
+ 
+fi
+done 
