36ebda296404e73f06812ca2ed722304635ad4a3
LaurenHuet <88010555+LaurenHuet@users.noreply.github.com>
2024-03-05 14:26:17 +0800

    Create filter_NCBI_farm.sh

---
+++ Decontamination/filter_NCBI_farm.sh
@@ -0,0 +1,13 @@
+rundir=
+DATE= [this is the date of the sequencing run as appears in the assembly file]
+scripts=
+for i in $rundir/*; do
+if [ -d "$i" ]; then
+sample=$(basename "$i")
+assembly=$(basename "$i.ilmn.$DATE")
+rundir=
+fasta="$rundir/$sample/assemblies/genome/$assembly.v129mh.fasta"
+sbatch $scripts/filter_NCBI.sh "$sample" "$rundir" "$assembly"
+ 
+fi
+done 
