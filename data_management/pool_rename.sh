9a2db4b9b7747f3182eee0e7e1b09da2493103eb
LaurenHuet <88010555+LaurenHuet@users.noreply.github.com>
2024-06-07 15:56:39 +0800

    Create pool_rename.sh

---
+++ data_management/pool_rename.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH -J pool_rename
+#SBATCH --time=12:00:00
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=64G
+#SBATCH --partition=work
+#SBATCH --clusters=setonix
+#SBATCH --account=pawsey0812
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=lauren.huet@uwa.edu.au
+
+
+module load singularity/3.11.4-nompi
+
+
+
+RUN=NEXT_221115_LP
+DIR=/scratch/pawsey0812/lhuet/NEXT_221115_LP
+SCRIPTS=/scratch/pawsey0812/lhuet/scripts
+
+
+for SAMPLE in $(cat $DIR/$RUN.forcat.prefix.txt); do
+echo "Running $SAMPLE" | tee -a $RUN.cat.log
+$SCRIPTS/wgs-cat.sh $DIR $RUN $SAMPLE && echo
+"$SAMPLE Complete" | tee -a $RUN.cat.log
+done 
