c1d28c9830fa126ce7e803d640c3c45a1362e0a2
Tyler Peirce <tyler.peirce@gmail.com>
2025-04-15 11:04:43 +0800

    replaced the date variable with wildcard to work when not running a batch from the same run

--- 02_assembly/02_genomescope-compile.sh
+++ 02_assembly/02_genomescope-compile.sh
@@ -10,7 +10,7 @@ TSV=$results/"$DATE"_genomescope_compiled_results.tsv
 echo Sample,Homozygosity,Heterozygosity,GenomeSize,RepeatSize,UniqueSize,ModelFit,ErrorRate | sed 's/,/\t/g' | tee $TSV
 
 
-for GSCOPE in $rundir/OG*/kmers/*$DATE*/*$DATE*-genomescope_summary.txt; do
+for GSCOPE in $rundir/OG*/kmers/*/*genomescope_summary.txt; do
 PREFIX=$(basename $GSCOPE | awk -F "-genomescope" '{print $1;}')
 sed -E 's/ [ ]+/\t/g' $GSCOPE | awk -F '\t' '{print $3;}' | tail -n 8 | awk '{print $1;}' | tr '\n' '\t' | sed 's/\t$/\n/' | sed "s/max/$PREFIX/" | tee -a $TSV
 done
