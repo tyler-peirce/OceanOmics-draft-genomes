5aed1f02df411defc6788956f7f7ab27b474c16d
Tyler Peirce <tyler.peirce@gmail.com>
2025-05-05 10:33:48 +0800

    updated pawsey user account

--- 05_backup_audit/02_final-genome-backup.sh
+++ 05_backup_audit/02_final-genome-backup.sh
@@ -15,19 +15,28 @@
 . ../configfile.txt
 
 # Zip the meryl database before backing up.
-for i in  "$rundir"/OG31; do
+for i in  "$rundir"/*; do
     if [ -d "$i" ]; then  # Check if the item in $rundir is a directory
         OG=$(basename "$i")
-        MERYL=$i/kmers/$OG.ilmn.$DATE.meryl
+        # Find the .meryl directory using 'find'
+        MERYL_DIR=$(find "$i/kmers" -maxdepth 1 -type d -name "*.meryl" | head -n 1)
+        echo "meryl $MERYL_DIR"
         
-        tar -czvf $MERYL.tar.gz -C $i/kmers/ $OG.ilmn.$DATE.meryl
-        
+        if [ -d "$MERYL_DIR" ]; then  # Now checking if it's a directory
+            MERYL_BASENAME=$(basename "$MERYL_DIR")  # Just the name without path
+            
+            echo "Archiving $MERYL_DIR into ${MERYL_DIR}.tar.gz"
+            
+            # tar the directory
+            tar -czvf "${MERYL_DIR}.tar.gz" -C "$(dirname "$MERYL_DIR")" "$MERYL_BASENAME"
+        else
+            echo "No .meryl directory found in $i/kmers/"
+        fi
+
         wait
         
-        rm -rf $MERYL
+        #rm -rf $MERYL_DIR
     fi
 done
 
-wait
-
-rclone move $rundir pawsey0812:oceanomics-genomes/genomes.v2/  --checksum -P
+#rclone move $rundir pawsey0964:oceanomics-draftgenomes/genomes.v2/  --checksum -P
