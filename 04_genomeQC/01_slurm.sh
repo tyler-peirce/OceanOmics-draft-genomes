b4ce6378fffc200612b0b2eb6b1c20471e901140
Tyler Peirce <tyler.peirce@gmail.com>
2025-05-27 16:15:50 +0800

    nmew updates

--- 04_genomeQC/01_slurm.sh
+++ 04_genomeQC/01_slurm.sh
@@ -1,30 +1,7 @@
 #!/bin/bash --login
-#SBATCH --account=pawsey0812
-#SBATCH --job-name=genomeQC
-#SBATCH --partition=work
-#SBATCH --cpus-per-task=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --time=24:00:00
-#SBATCH --mem=12G
-
-#SBATCH --export=NONE
-
-#-----------------
-
-. ../configfile.txt
 
 # Run these modules the first time you run the script, then comment out
-module load nextflow/
-module unload gcc
-module swap pawseyenv/2024.05 pawseyenv/2023.08
-module load gcc
-
-#Loading the required modules
-module load singularity/3.11.4-slurm
-
-# Setting the .nextflow to be stored on your scratch instead of in software
-unset SBATCH_EXPORT
-export NXF_HOME=$MYSCRATCH/.nextflow
+module load nextflow/24.04.3
 
 # Running the nextflow
 nextflow run genome.nf -c nextflow.config -profile setonix -resume -disable-jobs-cancellation -with-report genomeQC.html
